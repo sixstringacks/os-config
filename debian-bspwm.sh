79d4875ad930cad47af68a338080231a7ef54cbc
autocowrekt <autocowrekt@protonmail.com>
2021-04-02 18:23:49 -0400

    updates...

---
+++ debian-bspwm.sh
@@ -0,0 +1,6 @@
+# configure user, install sudo vim, add to sudo group
+
+# install bspwm
+sudo apt install xorg bspwm xterm feh compton polybar rofi lxappearance libnotify-bin dunst breeze-cursor-theme
+
+sudo pip3 install pywal
