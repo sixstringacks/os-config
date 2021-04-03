228b28de735d40481cd22e238fe92f7082833edb
autocowrekt <autocowrekt@protonmail.com>
2021-04-03 00:39:11 -0400

    minor updates

--- debian-bspwm.sh
+++ debian-bspwm.sh
@@ -4,3 +4,18 @@
 sudo apt install xorg bspwm xterm feh compton polybar rofi lxappearance libnotify-bin dunst breeze-cursor-theme -y
 
 sudo pip3 install pywal
+
+# Clone setup and dotfiles
+git clone https://github.com/autocowrekt/os-config.git
+
+# Cleanup for stow
+rm ~/.bashrc ~/.bash_profile
+
+# symlink dotfiles
+stow -d ~/os-config/dotfiles -t ~/ `ls ~/os-config/dotfiles`
+
+# Make files executable
+chmod 750 ~/.config/bspwm/bspwmrc
+chmod 750 ~/.config/sxhkd/sxhkdrc
+chmod 750 ~/.config/polybar/launch.sh
+chmod 750 ~/.config/dunst/dunstrc
