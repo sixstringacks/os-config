f7a60e9fcdf985831b8c0ce01fffc4ba2d4bbdea
infoplague <rppalmer@gmail.coM>
2021-04-03 20:45:21 -0400

    updated bspwmrc

--- debian-bspwm.sh
+++ debian-bspwm.sh
@@ -1,7 +1,7 @@
 # configure user, install sudo vim, add to sudo group
 
 # install bspwm
-sudo apt install xorg bspwm xterm feh compton polybar rofi lxappearance libnotify-bin dunst breeze-cursor-theme -y
+sudo apt install xorg bspwm xterm feh compton polybar rofi lxappearance libnotify-bin dunst notify-osd breeze-cursor-theme -y
 
 sudo pip3 install pywal
 
