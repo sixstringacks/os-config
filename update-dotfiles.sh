2fc2b9a93243aca0d9ce10178aede0324500d61d
infoplague <rppalmer@gmail.coM>
2021-04-03 21:00:59 -0400

    updated config

---
+++ update-dotfiles.sh
@@ -0,0 +1,7 @@
+cd ~/os-config
+git reset --hard
+git pull
+chmod 750 ~/.config/dunst/dunstrc
+chmod 750 ~/.config/polybar/launch.sh
+chmod 750 ~/.config/sxhkd/sxhkdrc
+chmod 750 ~/.config/bspwm/bspwmrc
